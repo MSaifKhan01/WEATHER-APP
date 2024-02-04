77a7b0ec9df4010187d91fbe8a2a3489aae90b2f
Mohd Saif Khan <msaifkhan5038@gmail.com>
2024-02-04 16:44:18 +0530

    Redis updated

--- my-react-app/src/Components/SeacrhCard.jsx
+++ my-react-app/src/Components/SeacrhCard.jsx
@@ -15,58 +15,47 @@ let token=localStorage.getItem("token")
 //     handleCityChange(city)
 //   }, [city]);
   
-  const handleCityChange = () => {
-    
-    console.log("yvv",token)
-    if(!token){
-      
-      Swal.fire("login First")
-      navigate("/login")
-     
+const handleCityChange = async () => {
+  try {
+    if (!token) {
+      Swal.fire("Login First");
+      navigate("/login");
+      return;
     }
-    console.log("city is : ",city)
-    
-    // if (city === '') return; 
-    fetch(`https://weather-app-sw7g.onrender.com/city?city=${city}`,{
-      headers:{
-        "Authorization":`${token}`
+
+    if (city === '') return;
+
+    const response = await fetch(`https://weather-app-sw7g.onrender.com/city?city=${city}`, {
+      headers: {
+        "Authorization": `${token}`
       }
-    })
-      .then(response => {
-        console.log("fhvn ")
-        if(response.statusText==="Too Many Requests"){
+    });
 
-           Swal.fire('You Have Make Too Many Request You need to Login again');
-           localStorage.removeItem("user")
-           localStorage.removeItem("token")
-           navigate("/login")
+    if (response.statusText === "Too Many Requests") {
+      Swal.fire('You Have Made Too Many Requests. Please Login again');
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      navigate("/login");
+      return;
+    }
 
-          console.log("don")
-        }
-        if (!response.ok) {
-          console.log(response)
-          throw new Error('Network response was not ok');
-        }
-        
-        
-        
-        let res= response.json();
-        console.log("Res : ",res)
-        return res;
-      })
-      .then(data => {
-        console.log("cgcgc",data);
-       
-        setWeatherData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  };
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    console.log("Response data:", data);
+    setWeatherData(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
 
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1 style={{ textAlign: 'center' }}>Weather App</h1>
+
       <div className='searchCont'>
       <div >
         <input className='inputTag'
