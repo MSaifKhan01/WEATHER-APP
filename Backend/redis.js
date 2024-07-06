13ac5a1f0faab8da556ea66ddc68203e8381331c
Mohd Saif Khan <msaifkhan5038@gmail.com>
2024-07-06 13:45:23 +0530

    Redis updated

--- Backend/redis.js
+++ Backend/redis.js
@@ -1,9 +1,11 @@
 const Redis=require('ioredis')
 
 const configuartion={
-    port:16520,
-    host:"redis-16520.c305.ap-south-1-1.ec2.cloud.redislabs.com",
-    
+    // port:16520,
+    port:   15072,
+    // host:"redis-16520.c305.ap-south-1-1.ec2.cloud.redislabs.com",
+    host:"redis-15072.c84.us-east-1-2.ec2.redns.redis-cloud.com",
+   
     username:"default",
     password:process.env.redis_Key
 }
